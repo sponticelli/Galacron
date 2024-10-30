ad582322ce1b65523ff3dcb6964b4ebd5c1fd041
Sandro Ponticelli <s.ponticelli@whatwapp.com>
2024-10-30 23:45:26 +0100

    feat: add some audio and gfx assets

---
+++ Assets/Galacron/Scripts/Editor/PostProcessors/SpriteImportSettings.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Galacron.PostProcessors
+{
+    public class SpriteImportSettings : AssetPostprocessor
+    {
+        private void OnPreprocessTexture()
+        {
+            if (assetPath.Contains("Sprites"))
+            {
+                TextureImporter importer = assetImporter as TextureImporter;
+                importer.filterMode = FilterMode.Point;
+                importer.textureCompression = TextureImporterCompression.Uncompressed;
+                importer.spritePixelsPerUnit = 16;
+                importer.mipmapEnabled = false;
+                importer.textureType = TextureImporterType.Sprite;
+                importer.spriteImportMode = SpriteImportMode.Single;
+            }
+        }
+    }
+}
