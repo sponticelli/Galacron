6003a858abf2a29261b4a1a886dea9b77be1df80
Sandro Ponticelli <s.ponticelli@whatwapp.com>
2024-11-03 22:13:50 +0100

    feat: integrate nexus sequence to build a WaveManager

--- Assets/Galacron/Scripts/Player/PlayerMovement.cs
+++ Assets/Galacron/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ namespace Galacron.Player
     {
         [Header("Settings")]
         [SerializeField] private float _speed;
+        [SerializeField] private float smoothTime = 0.1f;
      
         
         [Header("References")]
@@ -13,24 +14,33 @@ namespace Galacron.Player
         
         
         private float _horizontalMovement;
-        
+        private Vector3 velocity = Vector3.zero;
+        private Vector3 targetPosition;
+
+        private void Start()
+        {
+            targetPosition = _targetTransform.position;
+        }
+
 
         private void Update()
         {
-            _targetTransform.position += Vector3.right * (_horizontalMovement * _speed * Time.deltaTime);
+            targetPosition = _targetTransform.position;
+            targetPosition.x += _horizontalMovement * _speed * Time.deltaTime;
+            
+            _targetTransform.position = Vector3.SmoothDamp(
+                transform.position,
+                targetPosition,
+                ref velocity,
+                smoothTime
+            );
         }
-
         
-
         public void Move(float horizontalMovement)
         {
             _horizontalMovement = horizontalMovement;
         }
         
-        
-        
     }
-    
-    
     
 }
